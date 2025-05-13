ec8cb44ef2faab844c88f7467b9903715dc6d56c
farhankamal05 <146211775+farhankamal05@users.noreply.github.com>
2025-05-13 11:44:18 -0400

    Completed 111

--- Domain1/Student/111-str.py
+++ Domain1/Student/111-str.py
@@ -1,2 +1,8 @@
+import random
 first_name = "Jason"
 last_name = "Manibog"
+e = random.randint(1,2)
+if e == 1:
+    print(first_name + " " +last_name)
+else:
+    print(f"{last_name} {first_name}")
